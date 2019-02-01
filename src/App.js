377ee4d43234d72f971189e4ad5e4674afc0e7bb
trent <b.trent5@gmail.com>
2019-02-01 15:35:50 -0700

    updating links in production code

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Bindex</h1>
-        <a href="https://github.com/ESOF-423/Bindex/blob/master/Documentation/UserDocumentation.md" target="blank">User Documentation</a>
+        <a href="https://github.com/ESOF-423/BindexTK/blob/master/Documentation/UserDocumentation.md" target="blank">User Documentation</a>
         <br />
-        <a href="https://github.com/ESOF-423/Bindex/blob/master/Documentation/DeveloperDocumentation.md" target="blank">Developer Documentation</a>
+        <a href="https://github.com/ESOF-423/BindexTK/blob/master/Documentation/DeveloperDocumentation.md" target="blank">Developer Documentation</a>
       </div>
     );
   }
