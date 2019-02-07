1740930d60e525c4df996aa35025c9fd496913ca
LAPTOP-Q7OOIVSO\grace <gracewalkuski@gmail.com>
2019-02-06 18:48:38 -0700

    added material-ui to package-lock.json and turned the links into buttons

--- src/App.js
+++ src/App.js
@@ -1,17 +1,42 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from '@material-ui/core/styles';
+import "./App.css";
+
+const styles = {
+  root: {
+    width: '100%',
+    maxWidth: 500,
+  },
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Bindex</h1>
-        <a href="https://github.com/ESOF-423/BindexTK/blob/master/Documentation/UserDocumentation.md" target="blank">User Documentation</a>
-        <br />
-        <a href="https://github.com/ESOF-423/BindexTK/blob/master/Documentation/DeveloperDocumentation.md" target="blank">Developer Documentation</a>
+        <Typography component="h2" variant="h1" gutterBottom>
+          Bindex
+        </Typography>
+        <Button variant="contained">
+          <a
+            href="https://github.com/ESOF-423/BindexTK/blob/master/Documentation/UserDocumentation.md"
+            target="blank"
+          >
+            User Documentation
+          </a>
+        </Button>
+        <Button variant="contained">
+          <a
+            href="https://github.com/ESOF-423/BindexTK/blob/master/Documentation/DeveloperDocumentation.md"
+            target="blank"
+          >
+            Developer Documentation
+          </a>
+        </Button>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
