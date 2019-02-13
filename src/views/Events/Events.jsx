6e51bd0964eb2548525754724d20240b9283a762
Ian Hecker <ianhecker@yahoo.com>
2019-02-13 11:49:25 -0700

    Removed unnecessary imports from view jsx files

--- src/views/Events/Events.jsx
+++ src/views/Events/Events.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+
+// @material-ui/core
+import withStyles from "@material-ui/core/styles/withStyles";
+
 // core components
 import GridItem from "../../components/Grid/GridItem.jsx";
 import GridContainer from "../../components/Grid/GridContainer.jsx";
@@ -10,7 +14,7 @@ function Events(props) {
     <div>
       <GridContainer>
         <GridItem xs={12} sm={12} md={8}>
-          <p>Here in this tab maybe we can have a calendar application to show month and weekly views, with functionality to edit dates</p>
+          <p>Here in this tab maybe we can have a calendar application to show month and weekly views, with functionality to </p>
         </GridItem>
         <GridItem xs={12} sm={12} md={4}>
           
