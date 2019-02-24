eb78f4fa6b45cdcb0bec2a10776b657c2c0c2bb5
LAPTOP-Q7OOIVSO\grace <gracewalkuski@gmail.com>
2019-02-23 17:52:49 -0700

    added members page content

--- src/pages/Admin/AdminSidebar/AdminEventsPage/Events.jsx
+++ src/pages/Admin/AdminSidebar/AdminEventsPage/Events.jsx
@@ -15,99 +15,15 @@ import Table from "../../../../components/Table/Table.jsx";
 
 function Events() {
   return (
-    <div>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={4}>
-          <Card>
-            <CardHeader color="info">
-              <h4>Search</h4>
-              <p>Search for an event by name or date</p>
-            </CardHeader>
-            <form>
-              <CardBody>
-                <CustomInput
-                  id="date"
-                  formControlProps={{
-                    fullWidth: true
-                  }}
-                  inputProps={{
-                    type: "date"
-                  }}
-                />
-                <CustomInput
-                  id="name"
-                  formControlProps={{
-                    fullWidth: true
-                  }}
-                  inputProps={{
-                    type: "text",
-                    placeholder:"Event Name"
-                  }}
-                />
-              </CardBody>
-              <CardFooter>
-                <Button size="lg">
-                  Search
-                </Button>
-              </CardFooter>
-            </form>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={8}>
-          <Card>
-            <CardHeader color="info">
-              <h4>Results</h4>
-            </CardHeader>
+    <GridContainer>
+      <GridItem xs={12} sm={12} md={4}>
+        <Card>
+          <CardHeader color="info">
+            <h4>Search</h4>
+            <p>Search for an event by name or date</p>
+          </CardHeader>
+          <form>
             <CardBody>
-              <Table
-                tableHeaderColor="info"
-                tableHead={["Name", "Description", "Date", "Time", ""]}
-                tableData={[
-                  [
-                    <h5>Yoga</h5>,
-                    "A Hindu spiritual and ascetic discipline that is widely practiced for health and relaxation.",
-                    "2/21/2019",
-                    "5:00PM",
-                    "edit"
-                  ],
-                  [
-                    <h5>Dinner</h5>,
-                    "Delicious meals with friends and family.",
-                    "2/21/2019",
-                    "6:00PM",
-                    "edit"
-                  ]
-                ]}
-              />
-            </CardBody>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={12}>
-          <Card>
-            <CardHeader color="info">
-              <h4>Create Event</h4>
-            </CardHeader>
-            <CardBody>
-              <CustomInput
-                labelText="Event Name..."
-                id="name"
-                formControlProps={{
-                  fullWidth: true
-                }}
-                inputProps={{
-                  type: "name"
-                }}
-              />
-              <CustomInput
-                labelText="Event Description..."
-                id="description"
-                formControlProps={{
-                  fullWidth: true
-                }}
-                inputProps={{
-                  type: "description"
-                }}
-              />
               <CustomInput
                 id="date"
                 formControlProps={{
@@ -118,24 +34,102 @@ function Events() {
                 }}
               />
               <CustomInput
-                id="time"
+                id="name"
                 formControlProps={{
                   fullWidth: true
                 }}
                 inputProps={{
-                  type: "time"
+                  type: "text",
+                  placeholder: "Event Name"
                 }}
               />
             </CardBody>
             <CardFooter>
-              <Button size="lg">
-                Add Event
-              </Button>
+              <Button size="lg">Search</Button>
             </CardFooter>
-          </Card>
-        </GridItem>
-      </GridContainer>
-    </div>
+          </form>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={8}>
+        <Card>
+          <CardHeader color="info">
+            <h4>Results</h4>
+          </CardHeader>
+          <CardBody>
+            <Table
+              tableHeaderColor="info"
+              tableHead={["Name", "Description", "Date", "Time", ""]}
+              tableData={[
+                [
+                  <h5>Yoga</h5>,
+                  "A Hindu spiritual and ascetic discipline that is widely practiced for health and relaxation.",
+                  "2/21/2019",
+                  "5:00PM",
+                  "edit"
+                ],
+                [
+                  <h5>Dinner</h5>,
+                  "Delicious meals with friends and family.",
+                  "2/21/2019",
+                  "6:00PM",
+                  "edit"
+                ]
+              ]}
+            />
+          </CardBody>
+        </Card>
+      </GridItem>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="info">
+            <h4>Create Event</h4>
+          </CardHeader>
+          <CardBody>
+            <CustomInput
+              labelText="Event Name..."
+              id="name"
+              formControlProps={{
+                fullWidth: true
+              }}
+              inputProps={{
+                type: "name"
+              }}
+            />
+            <CustomInput
+              labelText="Event Description..."
+              id="description"
+              formControlProps={{
+                fullWidth: true
+              }}
+              inputProps={{
+                type: "description"
+              }}
+            />
+            <CustomInput
+              id="date"
+              formControlProps={{
+                fullWidth: true
+              }}
+              inputProps={{
+                type: "date"
+              }}
+            />
+            <CustomInput
+              id="time"
+              formControlProps={{
+                fullWidth: true
+              }}
+              inputProps={{
+                type: "time"
+              }}
+            />
+          </CardBody>
+          <CardFooter>
+            <Button size="lg">Add Event</Button>
+          </CardFooter>
+        </Card>
+      </GridItem>
+    </GridContainer>
   );
 }
 
